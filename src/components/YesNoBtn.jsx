39a41a8bdf867489c01cf8aa87d604d3af156631
adam.metal <adam.metal@codecool.com>
2022-01-20 10:17:00 +0100

    add the yes no button component

---
+++ src/components/YesNoBtn.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  outline: none;
+  border: 2px solid black;
+  border-radius: 3px;
+  color: white;
+  padding: 0.5rem 2rem;
+`;
+
+const YesButton = styled(Button)`
+  background-color: green;
+`;
+
+const NoButton = styled(Button)`
+  background-color: red;
+`;
+
+const YesNoBtn = (props) => {
+  return (
+    <div>
+      Do you need that kitten???
+      <div>
+        <YesButton onClick={props.onYes}>Yes</YesButton>
+        <NoButton onClick={props.onNo}>No</NoButton>
+      </div>
+    </div>
+  );
+};
+
+export default YesNoBtn;
