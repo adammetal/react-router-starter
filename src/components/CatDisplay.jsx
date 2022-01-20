dddf7d00f501d948d4fb181578380ebacb406fdb
adam.metal <adam.metal@codecool.com>
2022-01-20 10:57:00 +0100

    create a basic style and alignment for cats

--- src/components/CatDisplay.jsx
+++ src/components/CatDisplay.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import YesNoBtn from "./YesNoBtn";
+
+const CatBox = styled.div`
+  padding: 2rem;
+  border: 2px solid black;
+  border-radius: 3px;
+`;
+
+const CatImage = (props) => {
+  return (
+    <div>
+      <img src={props.src} alt="cat" />
+    </div>
+  );
+};
+
 const CatDisplay = () => {
-  return <div></div>;
+  const cat = {
+    id: "MTY0MTM2NQ",
+    url: "https://cdn2.thecatapi.com/images/MTY0MTM2NQ.gif",
+  };
+
+  return (
+    <CatBox>
+      <CatImage src={cat.url} />
+      <YesNoBtn />
+    </CatBox>
+  );
 };
 
 export default CatDisplay;
--- src/pages/CatStore.jsx
+++ src/pages/CatStore.jsx
@@ -1,3 +1,18 @@
-const CatStore = () => <h1>Cat Store</h1>;
+import styled from "styled-components";
+import CatDisplay from "../components/CatDisplay";
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const CatStore = () => (
+  <Flex>
+    <h1>Cat Store</h1>
+    <CatDisplay />
+  </Flex>
+);
 
 export default CatStore;
